4bf320644b7a15bfd6d3c503cf251d4479dc4abf
CyanNinja <67672152+CyanNinja@users.noreply.github.com>
2020-07-20 18:08:20 +1000

    Did another heap but with actual script and canvas. Still nothing to the puzzle generation algorithm.

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function setHeight() {
 // Puzzle Generation Algorithm
 
 // Show on Canvas
+	// Context
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+	// Example Background Colour
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, c.width, c.height);
+	// Example Text
+ctx.fillStyle = "#111";
+ctx.font = "64px Arial";
+ctx.fillText("I will be your image!", 256, 512);
